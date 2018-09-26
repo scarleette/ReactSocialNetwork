8d32afefbc544693b13f1bb7e3b50b750654fa50
Camila <camipereira2111@gmail.com>
2018-09-26 10:13:41 -0300

    prueba

--- src/components/Common/Navbar.js
+++ src/components/Common/Navbar.js
@@ -7,7 +7,7 @@ const NavBar = () => {
         <Navbar brand='Little Pet Society' right>
           {/* <NavItem onClick={() => console.log('test click')}>Getting started</NavItem*/}
           <NavItem divider />
-          <NavItem href='../Perfil/Perfil.js'><Icon>perm_identity</Icon></NavItem>
+          <NavItem href='../Perfil/Perfil.js'><Icon>perm_identity</Icon>holi</NavItem>
           <NavItem href=''><Icon>help_outline</Icon></NavItem>
           <NavItem href='../Muro/VistaMuro.js'><Icon>chrome_reader_mode</Icon></NavItem>
           <NavItem href=''><Icon>chat</Icon></NavItem>
