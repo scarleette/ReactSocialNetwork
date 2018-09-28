4affe52d8f9422af65f92945df398fc6690285cd
scarleette <scar.antoine@gmail.com>
2018-09-28 01:24:55 -0300

    Navbar con funcion para desloguearse

--- src/components/Common/Navbar.js
+++ src/components/Common/Navbar.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Navbar, NavItem, Icon } from 'react-materialize';
+import firebase from '../../firebase/firebase';
 
-const NavBar = () => {
+
+class NavbarC extends Component {
+ constructor() {
+   super();
+
+ }
+
+ logoutWithFirebase() {
+  firebase.auth().signOut()
+    .then(()=>{
+      console.log("Usuario finalizó su sesión");
+    })
+    .catch((error)=>{
+      console.log("Error de firebase > Código > "+error.code); //error.code nos mostrará el código de error para informarnos qué pasó
+      console.log("Error de firebase > Mensaje > "+error.message); //error.message nos mostrará el mensaje de firebase del mismo error
+    });
+}
+  render() {
     return (
       <div>
         <Navbar brand='Little Pet Society' right>
@@ -11,10 +29,12 @@ const NavBar = () => {
           <NavItem href={process.env.PUBLIC_URL + '/'}><Icon>help_outline</Icon></NavItem>
           <NavItem href={process.env.PUBLIC_URL + '/VistaMuro'}><Icon>chrome_reader_mode</Icon></NavItem>
           <NavItem href={process.env.PUBLIC_URL + '/'}><Icon>chat</Icon></NavItem>
-          <NavItem href={process.env.PUBLIC_URL + '/'}><Icon>close</Icon></NavItem>
+          <NavItem onClick = {this.logoutWithFirebase.bind(this)} ><Icon>close</Icon></NavItem>
         </Navbar>
       </div>
     )
-  };
+  }
+}
 
-export default NavBar;
+export default NavbarC;
+
